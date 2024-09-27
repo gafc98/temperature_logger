8cf22a9ef1f0cbfe118106a7dd2b5557125d687a
gafc98 <gafc@live.com.pt>
2024-09-27 21:24:06 +0200

    added if condition to check if services are already online

--- start_services.sh
+++ start_services.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-read -s -p "Enter password: " password
+if tmux has-session -t temperature_logger; then
+	echo "Temperature logging services are already running."
+else
+	read -s -p "Enter password: " password
+	tmux new-session -d -s temperature_logger 
+	tmux split-window -t temperature_logger
+	tmux split-window -h -t temperature_logger
+	tmux send-keys -t temperature_logger.1 "sudo ./logger -i2c_bus 2" ENTER
+	tmux send-keys -t temperature_logger.1 "$password" ENTER
+	tmux send-keys -t temperature_logger.0 "python webapp.py" ENTER
+	tmux send-keys -t temperature_logger.2 "python email_updater.py" ENTER
 
-tmux new-session -d -s temperature_logger 
-
-tmux split-window -t temperature_logger
-tmux split-window -h -t temperature_logger
-
-tmux send-keys -t temperature_logger.1 "sudo ./logger -i2c_bus 2" ENTER
-tmux send-keys -t temperature_logger.1 "$password" ENTER
-tmux send-keys -t temperature_logger.0 "python webapp.py" ENTER
-tmux send-keys -t temperature_logger.2 "python email_updater.py" ENTER
-
-
-echo "Temperature logging services started in the background."
-tmux list-sessions
+	echo "Temperature logging services started in the background."
+	tmux list-sessions
+fi
